cfd0b13684bc5694ca8dc844f7f6f89ecc12fa6f
defrost256 <danko.barna@gmail.com>
2018-09-22 09:54:11 +0200

    Comment for BikeWall

--- src/BikeWall.cpp
+++ src/BikeWall.cpp
@@ -14,8 +14,8 @@ BikeWall::~BikeWall()
 void BikeWall::ExtendLast(ofVec2f point)
 {
 	pointCount--;
-	poly.resize(pointCount);
-	AddNewLine(point);
+	poly.resize(pointCount);		//Remove the last point
+	AddNewLine(point);				//Add a new point, coz ofxIlda doesn't support moving points :/
 }
 
 void BikeWall::AddNewLine(ofVec2f point)
@@ -28,7 +28,7 @@ void BikeWall::Update(ofxIlda::Frame* frame, float soundShift)
 {
 	float hue, sat, val;
 	Color.getHsb(hue, sat, val);
-	poly.color = ofColor::fromHsb(hue + soundShift, sat, val);
+	poly.color = ofColor::fromHsb(hue + soundShift, sat, val);		//Calculate new color based on soundShift
 	frame->addPoly(poly);
 }
 
@@ -48,7 +48,7 @@ ofVec2f BikeWall::getClosestPointTo(ofVec2f p, float* outDistance)
 
 bool BikeWall::Intersects(ofRectangle shape)
 {
-	for (int i = 0; i < pointCount - 1; i++)
+	for (int i = 0; i < pointCount - 1; i++)			//check each line segment for intersection
 	{
 		if (shape.intersects(poly[i], poly[i + 1]))
 		{
--- src/BikeWall.h
+++ src/BikeWall.h
@@ -3,25 +3,59 @@
 #include "ofVec2f.h"
 #include "ofxIldaFrame.h"
 
+/**
+ * Class representing a wall drawn by bikes as a series of line segments.
+ */
 class BikeWall
 {
 public:
+	/**
+	 * Creates a new wall with the specified color.
+	 * @param c The color of the line.
+	 */
 	BikeWall(ofColor c);
 	~BikeWall();
 
+	/**
+	 * Moves the last point to a new position.
+	 * @param point The normalized position of the new point
+	 */
 	void ExtendLast(ofVec2f point);
+	/**
+	 * Adds a new point to the wall (thus adding a new line segment)
+	 * @param point The normalized position of the new point
+	 */
 	void AddNewLine(ofVec2f point);
 
+	/**
+	 * Adds the line segments to the Ilda point queue (frame)
+	 * @param frame The Ilda frame to add the points to.
+	 * @param soundShift The shift in hue produced by the sound engine
+	 */
 	void Update(ofxIlda::Frame* frame, float soundShift);
+	/**
+	 * Deletes the line segments
+	 */
 	void Clear();
 
+	/**
+	 * Returns the closest point on the wall to a specified point (in normalized coordinates)
+	 * @param p The point
+	 * @param outDistance Pointer to the distance, if needed (can be null)
+	 * @return The Point on the wall closest to the specified point.
+	 */
 	ofVec2f getClosestPointTo(ofVec2f p, float* outDistance = nullptr);
+	/**
+	 * Checks if a rectangle intersects the wall.
+	 * @param shape The rectangle
+	 * @return True, if the rectangle intersects the wall.
+	 */
 	bool Intersects(ofRectangle shape);
 
-	int BikeIdx;
+	int BikeIdx; ///< The index of the bike this wall belongs to.
 private:
 
-	ofColor Color;
-	ofxIlda::Poly poly;
-	int pointCount;
+	ofColor Color; ///< The base color of the wall (without sound shift)
+	ofxIlda::Poly poly; ///< The underlying polyline
+	int pointCount; ///< The number of points this wall has
 };
